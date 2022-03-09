fab9d7840458c347ebf7ff361fc5cd6fde595db5
Justin Morris <jnatmorris@gmail.com>
2022-03-09 22:10:09 +0100

    Added server types

--- types.ts
+++ types.ts
@@ -12,4 +12,27 @@ interface roomsType {
     [key: string]: string[]
 }
 
-export {usersType,gameRequestsType,roomsType }
+
+interface ServerToClientEvents {
+    allPlayableUsers: (arg: usersType[]) => void;
+    "inform-opponent-ofPlayer": (arg:gameRequestsType[])=> void
+}
+
+interface ClientToServerEvents {
+    requestDisconnect: () => void
+    sendMyName: (arg: string) => void
+    "request-Opponent": ({ }) => void
+    "remove-Opponent": ({ }) => void    
+    "both-Opponent": ({}) => void
+}
+
+interface InterServerEvents {
+    // none currently
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+export {usersType,gameRequestsType,roomsType,ServerToClientEvents,ClientToServerEvents,InterServerEvents,SocketData }
